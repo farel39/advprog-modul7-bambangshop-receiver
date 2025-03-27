740465ddfd47ab4c585ba6d5f2466d40469b4935
farel39 <argyafarel.kasyara@gmail.com>
2025-03-27 21:42:08 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -90,4 +90,9 @@ use std::thread;
             )),
         }
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
  }
